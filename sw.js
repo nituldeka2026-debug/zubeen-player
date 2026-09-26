9d35b37bfc10a4412672593edc4143a4aa75c1a3
nituldeka2026-debug <nitul.deka2026@gmail.com>
2026-09-26 19:42:13 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_V9='zubeen-radio-v11-stable';
-const APP=['/','/index.html','/style.css','/app.js','/schedule.html','/manifest.webmanifest','/icon.svg','/assets/zubeen-bg.jpg','/assets/zubeen-bg-wide.jpg'];
+const CACHE_V9='zubeen-radio-v10-photo';
+const APP=['/','/index.html','/style.css','/app.js','/schedule.html','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_V9).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_V9).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r.ok&&new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE_V9).then(c=>c.put(e.request,copy)).catch(()=>{})}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
